797201bb6e6824b97e0f0db8243cadd295075421
Flair Digital <flair.arg@gmail.com>
2024-08-01 17:01:16 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
             return false;
         }
 
-        const phone = '5491128067648';      
+        const phone = '5491128067715';      
 let msg = ' 🍀 ¡Hola! Quiero mi usuario y el bonus de bienvenida:\n';
         msg += '*Nombre:* ' + user.value + "\n";
        
